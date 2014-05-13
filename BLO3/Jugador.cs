9d0c1b26c017b4a8557df176f861f2d2d6bc7597
Juan López Rubio <j.lopez.r@gmail.com>
2014-05-13 08:57:50 +0200

    +++ Initial commit

---
+++ BLO3/Jugador.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLO3
+{
+    class Jugador
+    {
+        public String Nombre { get; set; }
+        public int Edad { get; set; }
+        public int Puntos { get; set; }
+
+
+        public Jugador(String nombre, int edad, int puntos)
+        {
+            this.Nombre = nombre;
+            this.Edad = edad;
+            this.Puntos = puntos;
+        }
+    }
+}
---
+++ BLO3/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace BLO3
+{
+    class Program
+    {
+        static Stack<String> salvarLista(string nombre, List<Jugador> lista, int n)
+        {
+            Stack<String> ret = new Stack<string>();
+            StreamWriter w = File.CreateText(nombre);
+
+            foreach (Jugador j in lista)
+            {
+                if (j.Puntos > n)
+                {
+                    ret.Push(j.Nombre);
+                    w.WriteLine(j.Nombre + " " + j.Edad + " "+ j.Puntos);
+                }
+            }
+            w.Close();
+
+            if (ret.Count > 0) 
+                return ret; 
+            else 
+                return null;
+        }
+
+        static void Main1(string[] args)
+        { 
+
+            //StreamReader r = File.OpenText("ganadores.txt");
+
+            List<Jugador> lista = new List<Jugador>();
+            int i = 0;
+            bool fin = false;
+            while (!fin)
+            {
+                String linea = Console.ReadLine();
+                //String linea = r.ReadLine();
+                if (linea == null || linea.Equals(""))
+                {
+                    fin = true;
+                } else { 
+                    String[] trozos = linea.Split(' ');
+                    Jugador j = new Jugador(trozos[0], Convert.ToInt32(trozos[1]), Convert.ToInt32(trozos[2]));
+                    lista.Add(j);
+                    i++;
+                }
+            }
+            Stack<String> resultado = salvarLista("ganadores.txt", lista, 50);
+            if (resultado != null)
+            {
+                foreach (String s in resultado)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+
+        }
+
+        static void Main2(String[] args)
+        {
+            List<Jugador> listaJugadores = new List<Jugador>();
+            Queue<Jugador> colaJugadores = new Queue<Jugador>();
+            Stack<Jugador> pilaJugadores = new Stack<Jugador>();
+
+            int i = 0;
+            while (i < 10)
+            {
+                Console.WriteLine("Escribe los datos del jugador separados por blancos");
+                String[] trozos = Console.ReadLine().Split(' ');
+                Jugador j = new Jugador(trozos[0], Convert.ToInt32(trozos[1]), Convert.ToInt32(trozos[2]));
+                listaJugadores.Add(j);
+                i++;
+            }
+
+            foreach (Jugador j in listaJugadores)
+            {
+                if (j.Puntos > 10)
+                {
+                    colaJugadores.Enqueue(j);
+                }
+                else
+                {
+                    pilaJugadores.Push(j);
+                }
+            }
+
+            while (pilaJugadores.Count > 0)
+            {
+                Jugador j = pilaJugadores.Pop();
+                Console.WriteLine("{0} {1} {2}", j.Nombre, j.Edad, j.Puntos);
+            }
+        }
+
+    }
+}
